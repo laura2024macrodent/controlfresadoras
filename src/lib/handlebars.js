26f6426c98764621221b6ded6548e22e71208dbe
Laura <l.forerocfmacrodent@gmail.com>
2025-02-24 16:08:05 -0500

    FE

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -3,15 +3,16 @@ const helpers = {};
 helpers.formatFecha = (fecha) => {
     // Convertir la fecha de string ISO a objeto Date
     const date = new Date(fecha);
-    
-    const localDate = new Date(date.getTime());
+
+    // Ajustar manualmente la hora para la zona horaria de Colombia (GMT-5)
+    const colombiaOffset = -5;  // GMT-5 para Colombia
+    const localDate = new Date(date.getTime() + colombiaOffset * 60 * 60 * 1000);
 
     // Devolver la fecha en el formato deseado
     return localDate.toLocaleDateString('es-CO', {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
-        timeZone: 'America/Bogota',
     });  // Ejemplo: 13 de noviembre de 2024
 };
 
@@ -24,8 +25,7 @@ helpers.formatHora = (fecha) => {
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit',
-        hour12: true,
-        timeZone: 'America/Bogota',
+        hour12: true
     });
 };
 
