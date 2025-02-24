816164465e6fc64df8ff5abe6a2902767c2d6f7b
Laura <l.forerocfmacrodent@gmail.com>
2025-02-24 16:38:04 -0500

    fecha

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -5,7 +5,7 @@ helpers.formatFecha = (fecha) => {
     const date = new Date(fecha);
 
     // Ajustar manualmente la hora para la zona horaria de Colombia (GMT-5)
-    const colombiaOffset = 5;  // Sumar 5 horas para Colombia
+    const colombiaOffset = 10;  // Sumar 5 horas para Colombia
     const localDate = new Date(date.getTime() + colombiaOffset * 60 * 60 * 1000);
 
     // Devolver la fecha en el formato deseado
